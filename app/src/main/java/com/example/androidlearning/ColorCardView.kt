437811f1d1984f0bf0f3518859c2c0f1ec6b13b1
Ömer Okumuş <omerokumus3@gmail.com>
2023-07-01 11:55:47 +0300

    ColorCard with viewData

--- app/src/main/java/com/example/androidlearning/ColorCardView.kt
+++ app/src/main/java/com/example/androidlearning/ColorCardView.kt
@@ -15,6 +15,9 @@ class ColorCardView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : LinearLayout(context, attrs) {
 
+    lateinit var colorCard: ImageView
+    lateinit var colorCode: TextView
+
     init {
         LayoutInflater.from(context).inflate(R.layout.color_card_view, this, true)
         inflate(context, R.layout.color_card_view, this)
@@ -22,12 +25,12 @@ class ColorCardView @JvmOverloads constructor(
 
     override fun onFinishInflate() {
         super.onFinishInflate()
+        colorCard = findViewById(R.id.color_image)
+        colorCode = findViewById(R.id.color_code)
         setup()
     }
 
     private fun setup() {
-        val colorCard: ImageView = findViewById(R.id.color_image)
-        val colorCode: TextView = findViewById(R.id.color_code)
         val r = Random.nextInt(256)
         val g = Random.nextInt(256)
         val b = Random.nextInt(256)
@@ -36,4 +39,23 @@ class ColorCardView @JvmOverloads constructor(
         colorCard.setBackgroundColor(color)
         colorCode.text = code
     }
+
+    fun customize(viewData: ColorCardViewData) {
+        val color = viewData.run {
+            Color.argb(a, r, g, b)
+        }
+        val code = viewData.run {
+            "#${Integer.toHexString(a)}${Integer.toHexString(r)}${Integer.toHexString(g)}${
+                Integer.toHexString(b)
+            }"
+        }
+        colorCard.setBackgroundColor(color)
+        colorCode.text = code
+    }
 }
+data class ColorCardViewData(
+    val a: Int,
+    val r: Int,
+    val g: Int,
+    val b: Int,
+)
--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -8,12 +8,14 @@ import com.example.androidlearning.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var status = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.card1.customize(ColorCardViewData(255, 122, 176, 12))
+        binding.card2.customize(ColorCardViewData(255, 213, 213, 99))
+        binding.card3.customize(ColorCardViewData(255, 23, 255, 189))
     }
 
 
