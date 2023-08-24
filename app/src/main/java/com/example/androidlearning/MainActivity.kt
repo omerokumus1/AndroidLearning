f549246c3b329ffc95e1ef1f19ac879c6ae52845
Ömer Okumuş <omerokumus3@gmail.com>
2023-08-24 07:54:28 +0300

    MainActivity cleanup

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -18,38 +18,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            val state = isAirplaneMode()
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                Settings.System.putInt(
-                    contentResolver,
-                    Settings.Global.AIRPLANE_MODE_ON,
-                    if (state) 0 else 1)
-
-                val intent = Intent(Settings.ACTION_AIRPLANE_MODE_SETTINGS)
-                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                startActivity(intent)
-
-            } else {
-                Settings.System.putInt(
-                    contentResolver,
-                    Settings.System.AIRPLANE_MODE_ON,
-                    if (state) 0 else 1)
-                val intent = Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-                intent.putExtra("state", !state)
-                sendBroadcast(intent)
-            }
-        }
-
     }
 
-    private fun isAirplaneMode(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            Settings.System.getInt(contentResolver, Settings.Global.AIRPLANE_MODE_ON, 0) == 1
-        } else {
-            Settings.System.getInt(contentResolver, Settings.System.AIRPLANE_MODE_ON, 0) == 1
-        }
-    }
 
 
 }
