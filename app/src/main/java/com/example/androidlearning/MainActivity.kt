f04227041b7b029e270f911cf166f92b1573bffb
Ömer Okumuş <omerokumus3@gmail.com>
2023-10-31 11:01:06 +0300

    bottom nav

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,24 +1,66 @@
 package com.example.androidlearning
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.androidlearning.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val firstFragment = FirstFragment()
+    private val secondFragment = SecondFragment()
+    private val thirdFragment = ThirdFragment()
+    private val fourthFragment = FourthFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        binding.button.setOnClickListener {
+//            it.layoutParams.height += 20 // height is -2 initally, why?
+//            it.layoutParams.width += 20 // width is -2 initally, why?
+//            it.requestLayout()
+//        }
+
+        supportFragmentManager.addFragmentOnAttachListener { fragmentManager, fragment ->
+            Log.d(
+                "onAttachListener",
+                "name: ${fragment.javaClass.name}, stackSize: ${fragmentManager.backStackEntryCount}"
+            )
+        }
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            Log.d("onAttachListener", "backstack changed: ${supportFragmentManager.fragments}")
+        }
 
 
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .replace(binding.fragmentContainer.id, firstFragment)
+            .addToBackStack(null)
+            .commit()
 
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.first -> replaceFragment(firstFragment)
+                R.id.second -> replaceFragment(secondFragment)
+                R.id.third -> replaceFragment(thirdFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .replace(binding.fragmentContainer.id, fragment)
+            .addToBackStack(fragment.javaClass.name)
+            .commit()
     }
 
 
