c1fee09a5da1c05a562a60e5cbd83f153355639d
Ömer Okumuş <omerokumus3@gmail.com>
2023-11-02 08:50:58 +0300

    custom view with inflation

--- app/src/main/java/com/example/androidlearning/BlueButtonView.kt
+++ app/src/main/java/com/example/androidlearning/BlueButtonView.kt
@@ -1,10 +1,15 @@
 package com.example.androidlearning
 
 import android.content.Context
+import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import com.example.androidlearning.databinding.ViewBlueButtonBinding
 
-class BlueButtonView(val context: Context){
+class BlueButtonView @JvmOverloads constructor(
+    val context: Context,
+    val attrs: AttributeSet? = null
+) : View(context, attrs) {
     var binding: ViewBlueButtonBinding
 
     init {
