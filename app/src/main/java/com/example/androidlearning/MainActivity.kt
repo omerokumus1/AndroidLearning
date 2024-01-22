074897375d1dff7a5f99fc67f498bb8fd39fb7b2
Ömer Okumuş <omerokumus3@gmail.com>
2024-01-22 12:00:19 +0300

    toast

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.androidlearning
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidlearning.databinding.ActivityMainBinding
 
@@ -12,15 +12,34 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.button.setOnClickListener {
+            val toast = Toast.makeText(this, "Toast message", Toast.LENGTH_SHORT)
+            // Requires API 30 / Android 11 / R
+            toast.addCallback(object : Toast.Callback() {
+                override fun onToastShown() {
+                    super.onToastShown()
+                    println("Toast shown")
+                }
+
+                override fun onToastHidden() {
+                    super.onToastHidden()
+                    println("Toast hidden")
+                }
+            })
+
+            toast.cancel()
+
+            toast.show()
+        }
 
 
     }
-
 
 
 }
