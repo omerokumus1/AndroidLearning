a3d0b3bd7cefc3b8e1b9b3a0dbe1d19f26e14188
Ömer Okumuş <omerokumus3@gmail.com>
2024-01-22 12:41:29 +0300

    snackbar

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.androidlearning
 
-import android.content.Intent
-import android.os.Build
+import android.content.res.ColorStateList
 import android.os.Bundle
-import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidlearning.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +17,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.button.setOnClickListener {
+
+            val snackbar = Snackbar
+                .make(binding.root, "Hello World", Snackbar.LENGTH_LONG)
+
+            snackbar.setAction("OK") {
+                snackbar.dismiss()
+            }
+            snackbar.setActionTextColor(resources.getColor(R.color.purple_200))
+
+            snackbar.setBackgroundTint(resources.getColor(R.color.purple_500))
+            snackbar.setTextColor(resources.getColor(R.color.teal_200))
+            snackbar.setTextMaxLines(1)
+
+            snackbar.addCallback(object : BaseTransientBottomBar.BaseCallback<Snackbar>() {
+                override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                    super.onDismissed(transientBottomBar, event)
+                }
+
+                override fun onShown(transientBottomBar: Snackbar?) {
+                    super.onShown(transientBottomBar)
+                }
+            })
+
+            snackbar.dismiss()
+
+
+
+            snackbar.show()
+
+
+        }
 
 
     }
-
 
 
 }
