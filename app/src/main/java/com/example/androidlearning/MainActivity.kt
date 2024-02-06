6323f6c878cd399241a293d08c374e06e3c2130a
Ömer Okumuş <omerokumus3@gmail.com>
2024-02-06 15:13:06 +0300

    gradient text color

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.androidlearning
 
-import android.content.Intent
-import android.os.Build
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
-import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidlearning.databinding.ActivityMainBinding
+import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +18,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val textView = binding.textView
+        val paint = textView.paint
+        val width = paint.measureText(textView.text.toString())
+
+        val textShader: Shader = LinearGradient(
+            0f, 0f, width, textView.textSize, intArrayOf(
+                Color.parseColor("#F97C3C"),
+                Color.parseColor("#FDB54E"),
+                Color.parseColor("#64B678"),
+                Color.parseColor("#478AEA"),
+                Color.parseColor("#8446CC")
+            ), null, Shader.TileMode.CLAMP
+        )
+        textView.paint.setShader(textShader)
 
 
     }
