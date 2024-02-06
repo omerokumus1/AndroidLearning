de8a271fa4ef75dca61c7eb8664013be38ccc163
Ömer Okumuş <omerokumus3@gmail.com>
2024-02-06 10:46:07 +0300

    toolbar/actionbar menu callbacks

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -3,20 +3,17 @@ package com.example.androidlearning
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.WindowInsets
-import android.view.WindowInsetsController
-import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat.FEATURE_ACTION_BAR
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import com.example.androidlearning.databinding.ActivityMainBinding
+import java.util.Timer
+import kotlin.concurrent.timerTask
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var menu: Menu
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,23 +23,54 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        this.menu = menu!!
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
     }
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
         menu?.removeItem(R.id.favorites)
-        return true
+        menu?.findItem(R.id.settings)
+            ?.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+        return super.onPrepareOptionsMenu(menu)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.favorites -> Toast.makeText(this, "Favorites clicked", Toast.LENGTH_SHORT).show()
-            R.id.addContact -> Toast.makeText(this, "Add contact clicked", Toast.LENGTH_SHORT)
-                .show()
+            R.id.favorites ->
+                Toast.makeText(this, "Favorites clicked", Toast.LENGTH_SHORT)
+                    .show()
+
+            R.id.addContact ->
+                Toast.makeText(this, "Add contact clicked", Toast.LENGTH_SHORT)
+                    .show()
         }
         return true
     }
+
+    // Does not work
+//    override fun onOptionsMenuClosed(menu: Menu?) {
+//        Toast.makeText(this, "Closed", Toast.LENGTH_SHORT).show()
+//        super.onOptionsMenuClosed(menu)
+//    }
+
+    override fun closeOptionsMenu() {
+        super.closeOptionsMenu()
+        Toast.makeText(this, "Closed", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun openOptionsMenu() {
+        super.openOptionsMenu()
+        Toast.makeText(this, "Opened", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun invalidateOptionsMenu() {
+        super.invalidateOptionsMenu()
+        Toast.makeText(this, "Invalidated", Toast.LENGTH_SHORT).show()
+    }
+
+
 
 
 }
