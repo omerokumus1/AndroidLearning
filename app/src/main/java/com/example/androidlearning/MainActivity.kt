69155dfcfdbf850f258e1eae57ca5482abb36e51
Ömer Okumuş <omerokumus3@gmail.com>
2024-02-07 17:59:11 +0300

    show keyboard

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.androidlearning
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidlearning.databinding.ActivityMainBinding
 
@@ -17,10 +15,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //? 1. Set focus to the EditText. May not work on the emulator
+        binding.editText.requestFocus()
 
+        //? 2. Show the keyboard. May not work on the emulator
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.editText, InputMethodManager.SHOW_IMPLICIT) // or SHOW_FORCED
 
     }
-
-
-
 }
