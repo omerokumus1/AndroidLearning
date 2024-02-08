ce62e5ff34ba9c3e863e8ffa72731ab26db73c1b
Ömer Okumuş <omerokumus3@gmail.com>
2024-02-08 15:05:30 +0300

    submenu

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.SubMenuBuilder
 import com.example.androidlearning.databinding.ActivityMainBinding
 
 
@@ -21,6 +24,22 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        menu?.addSubMenu("Submenu")?.also {
+            it.add(1, Menu.NONE, Menu.NONE, "Item 1.1")
+            it.add(1, Menu.NONE, Menu.NONE, "Item 1.2")
+            it.add(2, Menu.NONE, Menu.NONE, "Item 2.1")
+            it.add(2, Menu.NONE, Menu.NONE, "Item 2.2")
+        }
 
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        super.onPrepareOptionsMenu(menu)
+
+        return true
+    }
 
 }
