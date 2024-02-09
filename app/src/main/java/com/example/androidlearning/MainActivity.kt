30c9d77668f11ed2c291f0c804b191d6b3d03bb8
Ömer Okumuş <omerokumus3@gmail.com>
2024-02-09 10:41:52 +0300

    floating context menu

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidlearning.databinding.ActivityMainBinding
 
@@ -17,10 +21,55 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//        registerForContextMenu(binding.row)
 
+
+        binding.row.setOnCreateContextMenuListener { menu, view, menuInfo ->
+            menuInflater.inflate(R.menu.floating_context_menu, menu)
+
+            menu.getItem(0).setOnMenuItemClickListener { item ->
+                Toast.makeText(this, "Favorites Clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            menu.getItem(1).setOnMenuItemClickListener { item ->
+                Toast.makeText(this, "Share Clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+        }
+
+        binding.row2.setOnCreateContextMenuListener { menu, view, menuInfo ->
+            menuInflater.inflate(R.menu.floating_context_menu, menu)
+
+        }
 
     }
 
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        if (v == binding.row) {
+            menuInflater.inflate(R.menu.floating_context_menu, menu)
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_favorites -> {
+                Toast.makeText(this, "Favorites Clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.item_share -> {
+                Toast.makeText(this, "Share Clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
 
 
 }
