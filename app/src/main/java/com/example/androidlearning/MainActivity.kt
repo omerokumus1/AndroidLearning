9ec2b3d49db91b3eb61d4d7615dbbfec3569ca59
Ömer Okumuş <omerokumus3@gmail.com>
2024-06-22 13:14:56 +0300

    viewModel onCleared

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.androidlearning
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidlearning.databinding.ActivityMainBinding
 
@@ -11,12 +9,15 @@ import com.example.androidlearning.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainActivityViewModel>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        println(viewModel)
 
 
     }
---
+++ app/src/main/java/com/example/androidlearning/MainActivityViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.androidlearning
+
+import androidx.lifecycle.ViewModel
+
+
+class MainActivityViewModel : ViewModel() {
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+
+}
+
