da6b10a2b5328f79d9b9a59f63d9a8d83ebfaff4
Ömer Okumuş <omerokumus3@gmail.com>
2024-09-14 19:47:40 +0300

    viewModel creation & observation

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.androidlearning
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.androidlearning.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel by viewModels<MainActivityViewModel>()
+//    private val viewModel by viewModels<MainActivityViewModel>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +21,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        println(viewModel)
+        //* Create a ViewModel the first time the system
+        //* calls an activity's onCreate() method.
+        //* Re-created activities receive the same MainActivityViewModel
+        //* instance created by the first activity.
 
+        //* Use the 'by viewModels()' Kotlin property delegate
+        //* from the activity-ktx artifact
+        val viewModel: MainActivityViewModel by viewModels()
 
+        viewModel.uiState.observe(this) { uiState ->
+            println(uiState)
+        }
     }
 
 
--- app/src/main/java/com/example/androidlearning/MainActivityViewModel.kt
+++ app/src/main/java/com/example/androidlearning/MainActivityViewModel.kt
@@ -1,13 +1,41 @@
 package com.example.androidlearning
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlin.random.Random
+
+data class DiceUiState(
+    val firstDieValue: Int? = null,
+    val secondDieValue: Int? = null,
+    val numberOfRolls: Int = 0,
+)
 
 
 class MainActivityViewModel : ViewModel() {
 
+    // Expose screen UI state
+    private val _uiState = MutableLiveData(DiceUiState())
+    val uiState: LiveData<DiceUiState> get() = _uiState
+
+    // Handle business logic
+    fun rollDice() {
+        _uiState.value =
+            _uiState.value?.copy(
+                firstDieValue = Random.nextInt(from = 1, until = 7),
+                secondDieValue = Random.nextInt(from = 1, until = 7),
+                numberOfRolls = _uiState.value?.numberOfRolls?.plus(1) ?: 1
+            )
+    }
+
     override fun onCleared() {
         super.onCleared()
     }
+
 
 }
 
