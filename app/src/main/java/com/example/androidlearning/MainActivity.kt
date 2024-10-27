d7946e7ee436d95f86cae6992d97985927b4b52a
Ömer Okumuş <omerokumus3@gmail.com>
2024-10-27 07:45:26 +0300

    bitmap not finished

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.androidlearning
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.set
 import com.example.androidlearning.databinding.ActivityMainBinding
 
 
@@ -17,10 +20,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val width = 100
+        val height = 100
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
 
+        if (bitmap.isMutable) {
+//            repeat(width) { x ->
+//                repeat(height) { y ->
+//                //  bitmap.setPixel(x, y, Color.BLACK)
+//                    bitmap[x, y] = Color.BLACK
+//                }
+//            }
+
+            bitmap.setPixels(
+                intArrayOf(
+                    Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW,
+                    Color.CYAN, Color.MAGENTA, Color.LTGRAY, Color.DKGRAY
+                ), 0, 2, 0, 0, 2, 10
+            )
+
+        }
+
+        binding.imageView.setImageBitmap(bitmap)
 
     }
-
 
 
 }
